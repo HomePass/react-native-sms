f854fa947e7f1a25243fc748df4724577f71851c
Chu Ming Yeoh <chu@homepass.com>
2018-06-06 14:25:36 +1000

    automatically sends sms on android

--- android/src/main/java/com/tkporter/sendsms/SendSMSModule.java
+++ android/src/main/java/com/tkporter/sendsms/SendSMSModule.java
@@ -1,11 +1,10 @@
 package com.tkporter.sendsms;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Build;
-import android.provider.Telephony;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.telephony.SmsManager;
+import android.text.TextUtils;
 
-import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
@@ -13,16 +12,14 @@ import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.Callback;
 
-public class SendSMSModule extends ReactContextBaseJavaModule implements ActivityEventListener {
+import java.util.ArrayList;
 
+public class SendSMSModule extends ReactContextBaseJavaModule {
     private final ReactApplicationContext reactContext;
-    private Callback callback = null;
-    private static final int REQUEST_CODE = 5235;
 
     public SendSMSModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
-        reactContext.addActivityEventListener(this);
     }
 
     @Override
@@ -30,76 +27,44 @@ public class SendSMSModule extends ReactContextBaseJavaModule implements Activit
         return "SendSMS";
     }
 
-
-    @Override
-    public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
-        //System.out.println("in module onActivityResult() request " + requestCode + " result " + resultCode);
-        //canceled intent
-        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_CANCELED) {
-            sendCallback(false, true, false);
-        }
-    }
-
-    @Override
-    public void onNewIntent(Intent intent) {
-
-    }
-
-    public void sendCallback(Boolean completed, Boolean cancelled, Boolean error) {
-        if (callback != null) {
-            callback.invoke(completed, cancelled, error);
-            callback = null;
-        }
-    }
-
     @ReactMethod
     public void send(ReadableMap options, final Callback callback) {
         try {
-            this.callback = callback;
-            new SendSMSObserver(reactContext, this, options).start();
-
-            String body = options.hasKey("body") ? options.getString("body") : "";
+            SmsManager manager = SmsManager.getDefault();
+            String message = options.hasKey("body") ? options.getString("body") : "";
             ReadableArray recipients = options.hasKey("recipients") ? options.getArray("recipients") : null;
 
-            Intent sendIntent;
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                String defaultSmsPackageName = Telephony.Sms.getDefaultSmsPackage(reactContext);
-                sendIntent = new Intent(Intent.ACTION_SEND);
-                if (defaultSmsPackageName != null){
-                    sendIntent.setPackage(defaultSmsPackageName);
-                }
-                sendIntent.setType("text/plain");
-            }else {
-                sendIntent = new Intent(Intent.ACTION_VIEW);
-                sendIntent.setType("vnd.android-dir/mms-sms");
+            if(!isSmsFeatureSupported(reactContext) || !isSmsMessageValid(message)) {
+                callback.invoke(false, false, true);
+                return;
             }
 
-            sendIntent.putExtra("sms_body", body);
-            sendIntent.putExtra(sendIntent.EXTRA_TEXT, body);
-            sendIntent.putExtra("exit_on_sent", true);
+            ArrayList<String> messageParts = manager.divideMessage(message);
 
             //if recipients specified
             if (recipients != null) {
-                //Samsung for some reason uses commas and not semicolons as a delimiter
-                String separator = ";";
-                if(android.os.Build.MANUFACTURER.equalsIgnoreCase("Samsung")){
-                    separator = ",";
-                }
-                String recipientString = "";
                 for (int i = 0; i < recipients.size(); i++) {
-                    recipientString += recipients.getString(i);
-                    recipientString += separator;
+                    String recipient = recipients.getString(i);
+                    if(!TextUtils.isEmpty(recipient)) {
+                        manager.sendMultipartTextMessage(recipient, null, messageParts, null, null);
+                    }
                 }
-                sendIntent.putExtra("address", recipientString);
             }
 
-            reactContext.startActivityForResult(sendIntent, REQUEST_CODE, sendIntent.getExtras());
-        } catch (Exception e) {
+            callback.invoke(true, false, false);
+        } catch (UnsupportedOperationException e) {
             //error!
-            sendCallback(false, false, true);
+            callback.invoke(false, false, true);
             throw e;
         }
+    }
+
+    public boolean isSmsFeatureSupported(Context context) {
+        return context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_TELEPHONY);
+    }
+
+    public boolean isSmsMessageValid(String message) {
+        return !TextUtils.isEmpty(message);
     }
 
 }
--- android/src/main/java/com/tkporter/sendsms/SendSMSPackage.java
+++ android/src/main/java/com/tkporter/sendsms/SendSMSPackage.java
@@ -1,15 +1,11 @@
 package com.tkporter.sendsms;
 
-import android.app.Activity;
-
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
-import android.content.Intent;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -17,14 +13,10 @@ import java.util.List;
 public class SendSMSPackage implements ReactPackage {
 
     private SendSMSModule sendSms = null;
-    private static SendSMSPackage instance = null;
 
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactApplicationContext) {
         sendSms = new SendSMSModule(reactApplicationContext);
-        if (instance == null) {
-            instance = this;
-        }
         return Arrays.<NativeModule>asList(sendSms);
     }
 
@@ -35,17 +27,5 @@ public class SendSMSPackage implements ReactPackage {
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
-    }
-
-    public static SendSMSPackage getInstance() {
-        if (instance == null) {
-            instance = new SendSMSPackage();
-        }
-
-        return instance;
-    }
-
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        sendSms.onActivityResult(null, requestCode, resultCode, data);
     }
 }
